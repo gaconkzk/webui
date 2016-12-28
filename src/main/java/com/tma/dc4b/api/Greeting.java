c9b32072c6f0aae97e02199d7808cff8363e2bf3
tntvu <tntvu@tma.com.vn>
2016-12-28 11:44:18 +0700

    done tutorial part - 1

---
+++ src/main/java/com/tma/dc4b/api/Greeting.java
@@ -0,0 +1,23 @@
+package com.tma.dc4b.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ *
+ */
+@RestController
+public class Greeting {
+    @GetMapping("/greeting")
+    public Map<String, Object> greeting() {
+        Map<String, Object> model = new HashMap<>(  );
+        model.put( "id", UUID.randomUUID().toString() );
+        model.put( "content", "okie baby! like thissssss! "+model.get("id") );
+
+        return model;
+    }
+}
