a12b02759a31332bf7ecc9c9e352513274b00335
tntvu <tntvu@tma.com.vn>
2016-12-29 08:42:22 +0700

    add csrf

--- src/main/java/com/tma/dc4b/config/SecurityConfig.java
+++ src/main/java/com/tma/dc4b/config/SecurityConfig.java
@@ -6,26 +6,31 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 /**
  *
  */
 @Configuration
-@Order( SecurityProperties.ACCESS_OVERRIDE_ORDER)
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure( HttpSecurity http ) throws Exception {
-        http
-                .httpBasic()
-		    .and()
-			.authorizeRequests()
-                .antMatchers("/", "/index.html").permitAll()
-                .anyRequest().authenticated();
-    }
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web
-                .ignoring().antMatchers("/css/**", "images/**", "/resources/**");
-    }
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+        .httpBasic()
+        .and()
+        .authorizeRequests()
+        .antMatchers("/", "/index.html").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .csrf()
+        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+  }
+
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    web
+        .ignoring().antMatchers("/css/**", "images/**", "/resources/**");
+  }
 }
