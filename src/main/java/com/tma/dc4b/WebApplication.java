f11e110eb3e4985a0d7b4f17383dfd2f910cb04e
tntvu <tntvu@tma.com.vn>
2017-01-04 12:35:56 +0700

    done oauth2 authen with inmemory solution

--- src/main/java/com/tma/dc4b/WebApplication.java
+++ src/main/java/com/tma/dc4b/WebApplication.java
@@ -2,29 +2,37 @@ package com.tma.dc4b;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableOAuth2Sso
-public class WebApplication extends WebSecurityConfigurerAdapter {
+public class WebApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(WebApplication.class, args);
   }
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http
-        .logout().and()
-        .authorizeRequests()
-        .antMatchers("/index.html", "/", "/login").permitAll()
-        .anyRequest().authenticated().and()
-        .csrf()
+  @Configuration
+  @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+  protected static class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+      http
+          .logout().and()
+          .authorizeRequests()
+          .antMatchers("/index.html", "/", "/login").permitAll()
+          .anyRequest().authenticated().and()
+          .csrf()
           .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+    }
   }
 }
