b9e675f49f4038a317d8f908baa44cec90520350
tntvu <tntvu@tma.com.vn>
2017-01-04 13:38:40 +0700

    done login/logout - only logout at client

--- src/main/java/com/tma/dc4b/WebApplication.java
+++ src/main/java/com/tma/dc4b/WebApplication.java
@@ -7,32 +7,36 @@ import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableOAuth2Sso
-public class WebApplication {
+public class WebApplication extends WebSecurityConfigurerAdapter {
 
   public static void main(String[] args) {
     SpringApplication.run(WebApplication.class, args);
   }
 
-  @Configuration
-  @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-  protected static class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-      http
-          .logout().and()
-          .authorizeRequests()
-          .antMatchers("/index.html", "/", "/login").permitAll()
-          .anyRequest().authenticated().and()
-          .csrf()
-          .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
-    }
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+        .logout()
+          .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.ACCEPTED))
+          .deleteCookies("JSESSIONID")
+          .invalidateHttpSession(true).clearAuthentication(true).and()
+        .authorizeRequests()
+        .antMatchers("/index.html", "/", "/login").permitAll()
+        .anyRequest().authenticated().and()
+        .csrf()
+        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
   }
 }
