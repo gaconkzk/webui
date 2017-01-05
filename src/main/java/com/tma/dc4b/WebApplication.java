defa135e75f251cb104d30ed9c61bd77b73e8834
tntvu <tntvu@tma.com.vn>
2017-01-05 09:39:20 +0700

    Done #2, use jks in oauth2.

--- src/main/java/com/tma/dc4b/WebApplication.java
+++ src/main/java/com/tma/dc4b/WebApplication.java
@@ -1,21 +1,22 @@
 package com.tma.dc4b;
 
+import java.util.Map;
+import javax.inject.Inject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.cloud.client.loadbalancer.RestTemplateCustomizer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -38,5 +39,23 @@ public class WebApplication extends WebSecurityConfigurerAdapter {
         .anyRequest().authenticated().and()
         .csrf()
         .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+  }
+
+  @Bean
+  public RestTemplate loadBalancedRestTemplate(RestTemplateCustomizer customizer) {
+    RestTemplate restTemplate = new RestTemplate();
+    customizer.customize(restTemplate);
+    return restTemplate;
+  }
+
+  @Inject
+  private RestTemplate keyUriRestTemplate;
+
+  private String getKeyFromAuthorizationServer() {
+    HttpEntity<Void> request = new HttpEntity<>(new HttpHeaders());
+    return (String) this.keyUriRestTemplate
+        .exchange("http://localhost:9999/oauth/token_key", HttpMethod.GET, request, Map.class)
+        .getBody()
+        .get("value");
   }
 }
