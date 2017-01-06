7634ac0c32aee40237f8f8fd8ae1622a3fbbfb0a
tntvu <tntvu@tma.com.vn>
2017-01-06 13:55:02 +0700

    finally, currently there is no logout mechanism with jwt implementation. We need other way.

--- src/main/java/com/tma/dc4b/WebApplication.java
+++ src/main/java/com/tma/dc4b/WebApplication.java
@@ -1,12 +1,20 @@
 package com.tma.dc4b;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @SpringBootApplication
@@ -22,15 +30,34 @@ public class WebApplication extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
-        .logout().clearAuthentication(false).addLogoutHandler((req, res, auth) -> {
-      //serverLogout(req,res);
+        .logout().clearAuthentication(true).invalidateHttpSession(true)
+        .addLogoutHandler((req, res, auth) -> {
+          serverLogout(req, res);
     }).logoutSuccessHandler((req, res, auth) -> {
 
-    }).and()
+    }).deleteCookies("JSESSIONID").and()
         .authorizeRequests()
         .antMatchers("/index.html", "/", "/login").permitAll()
         .anyRequest().authenticated().and()
         .csrf()
         .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+  }
+
+  @Autowired
+  OAuth2ClientContext clientContext;
+
+  @Autowired
+  OAuth2ProtectedResourceDetails protectedResourceDetails;
+
+  public OAuth2RestOperations oAuth2RestOperations() {
+    OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(protectedResourceDetails,
+        clientContext);
+    return restTemplate;
+  }
+
+  private void serverLogout(HttpServletRequest req, HttpServletResponse res) {
+    oAuth2RestOperations()
+        .exchange("http://localhost:9999/uaa/logout", HttpMethod.POST, null, Void.class);
+    clientContext.getAccessToken();
   }
 }
